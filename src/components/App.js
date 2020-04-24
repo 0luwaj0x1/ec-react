c5143f8c57dbdd33656aca7c91a30c6c47b11937
jilesanmi <ilesanmi.josiah@gmail.com>
2020-04-24 16:16:24 +0100

    configure index page for router

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 import Sidebar from "./Sidebar";
 import QuestionOne from "./QuestionOne";
