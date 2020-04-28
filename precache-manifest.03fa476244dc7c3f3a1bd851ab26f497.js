ab9a9423a3c4432f7969b0601a415346555c409a
jilesanmi <ilesanmi.josiah@gmail.com>
2020-04-28 15:51:44 +0100

    Updates

---
+++ precache-manifest.03fa476244dc7c3f3a1bd851ab26f497.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68cd2a2333f44e8d46e7fda74ee9b490",
+    "url": "/ec-react/index.html"
+  },
+  {
+    "revision": "0cb1ce53fa0d052a13d0",
+    "url": "/ec-react/static/css/main.3eb85504.chunk.css"
+  },
+  {
+    "revision": "a0e58c4e5d7cde485e6a",
+    "url": "/ec-react/static/js/2.60976949.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ec-react/static/js/2.60976949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb1ce53fa0d052a13d0",
+    "url": "/ec-react/static/js/main.3ad529c1.chunk.js"
+  },
+  {
+    "revision": "c21affb8ecfd0d435160",
+    "url": "/ec-react/static/js/runtime-main.7dde968a.js"
+  }
+]);
